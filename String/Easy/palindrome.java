c754da80eb13d2f4beb42fcecfe8b6d278ee9192
sohagannamA <113580160+sohagannamA@users.noreply.github.com>
2023-01-17 00:25:46 +0600

    Update palindrome.java

---
+++ String/Easy/palindrome.java
@@ -0,0 +1,14 @@
+package Easy;
+class test2{
+        public static boolean IsPalindrome(String mystring){
+                for(int i=0;i<mystring.length()/2;i++){
+                        if(mystring.charAt(i)!=mystring.charAt(mystring.length()-1-i)){
+                               return false;
+                        }
+                }
+                return true;
+        }
+        public static void main(String[] args) {
+               System.out.println(IsPalindrome("abba"));
+        }
+}
